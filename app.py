60b146d291ee142341337dd3c5d2bc9ac5cbb8f8
Marco-Lu-76 <marcoludwig31051976@gmail.com>
2025-05-17 12:42:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,4 @@ frage = st.text_input("Stelle eine Frage basierend auf deinen Dokumenten:")
 
 if frage:
     antwort = ask_question(frage)
-import streamlit as st
-from chatbot.query_engine import ask_question
-
-st.title("Mein lokaler GPT-Chatbot")
-frage = st.text_input("Stelle eine Frage basierend auf deinen Dokumenten:")
-
-if frage:
-    antwort = ask_question(frage)
     st.write(antwort)
